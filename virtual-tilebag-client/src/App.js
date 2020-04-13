c3e637a7e390a6aa2e13532457601d8311fd5214
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-13 20:37:16 +1200

    Routes

--- virtual-tilebag-client/src/App.js
+++ virtual-tilebag-client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-// import BookContextProvider from './contexts/BookContext'; // remove
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Dashboard from './components/Dashboard';
-import About from './components/About';
-import Contact from './components/Contact';
-import './App.css';
+import React, { Component } from "react";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+const users = [{ name: `Param` }, { name: `Vennila` }, { name: `Afrin` }];
 
 const App = () => {
   return (
-    // <BookContextProvider> // remove
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-      </Switch>
-    </BrowserRouter>
-    // </BookContextProvider> // remove
+    <section className="App">
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/new-game" component={NewGame} />
+        <Route exact path="/game/id:gameID/p:player" component={Game} />
+      </Router>
+    </section>
   );
+};
+
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
 };
 
 export default App;
