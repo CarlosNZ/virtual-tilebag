24f29306f67a06f71f77557bcbd73399d67b521a
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-13 22:06:50 +1200

    NewGame basics

--- virtual-tilebag-client/src/App.js
+++ virtual-tilebag-client/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
-
-const users = [{ name: `Param` }, { name: `Vennila` }, { name: `Afrin` }];
+import { shuffleBag, drawFromBag } from "./tilebag";
+import { NewGame } from "./NewGame";
 
 const App = () => {
   return (
@@ -14,14 +13,21 @@ const App = () => {
         <Link to="/users">Users</Link>
         <Route exact path="/" component={Home} />
         <Route exact path="/new-game" component={NewGame} />
-        <Route exact path="/game/id:gameID/p:player" component={Game} />
+        {/* <Route exact path="/game/id:gameID/p:player" component={Game} /> */}
       </Router>
     </section>
   );
 };
 
-const IndexPage = () => {
-  return <h3>Home Page</h3>;
+const Home = () => {
+  const bag = shuffleBag(1234);
+
+  return (
+    <div>
+      <h3>Home Page</h3>
+      <p>{drawFromBag(bag, 100, 7)}</p>
+    </div>
+  );
 };
 
 export default App;
