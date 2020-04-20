2252c9e9d4855b07e456758ad394e0a76174f50d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-20 18:40:11 +1200

    Bit of a tidy up

--- virtual-tilebag-client/src/components/Game.js
+++ virtual-tilebag-client/src/components/Game.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import * as FirestoreDb from "../services/firebase";
-import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import { shuffleBag, drawFromBag } from "../services/tilebag";
 
 export const Game = (props) => {
@@ -8,22 +7,12 @@ export const Game = (props) => {
 
   // Game constants
   const gameId = props.match.params.gameID;
-  const thisPlayer = props.match.params.player;
+  const thisPlayer = parseInt(props.match.params.player);
   const tileBag = shuffleBag(gameId);
-
-  let tilesRemaining;
-  let currentPlayer;
-
-  const [gameLoaded, setGameLoaded] = useState(true);
 
   return (
     <div>
-      <h1>This will render first</h1>
-      {gameLoaded && (
-        <div>
-          <Rack gameId={gameId} thisPlayer={thisPlayer} tileBag={tileBag} />
-        </div>
-      )}
+      <Rack gameId={gameId} thisPlayer={thisPlayer} tileBag={tileBag} />
     </div>
   );
 };
@@ -31,7 +20,6 @@ export const Game = (props) => {
 const Rack = (props) => {
   console.log("Rack re-render");
   // State values
-  // const [rack, setRack] = useState(["", "", "", "", "", "", ""]);
   const [rackSelectedIndices, setRackSelectedIndices] = useState(new Set());
   const [gameData, setGameData] = useState({});
   const [players, setPlayers] = useState([]);
@@ -41,27 +29,25 @@ const Rack = (props) => {
   useEffect(() => {
     const unsubscribe = FirestoreDb.syncGameState(props.gameId, (doc) => {
       setGameData({
-        currentPlayer: doc.data().currentPlayer,
-        tilesRemaining: doc.data().tilesRemaining,
+        currentPlayer: parseInt(doc.data().currentPlayer),
+        tilesRemaining: parseInt(doc.data().tilesRemaining),
       });
       setPlayers(doc.data().players);
-      // setRack(stringToRack(doc.data().racks[props.thisPlayer - 1]));
       setRacks(doc.data().racks);
       console.log("Updating from database");
-      // console.log(gameData);
     });
     return unsubscribe;
   }, []);
 
-  // Update hasGameStarted based on all players' rack state
+  // Once all players have drawn initial tiles, game becomes ready
   useEffect(() => {
-    if (racks.filter((r) => r !== "").length === players.length && players.length > 0 && gameData.currentPlayer == 0) {
-      FirestoreDb.updateDatabaseState(props.gameId, "currentPlayer", 1);
+    if (racks.filter((r) => r !== "").length === players.length && players.length > 0 && gameData.currentPlayer === 0) {
+      FirestoreDb.updateState(props.gameId, "currentPlayer", 1);
       console.log("Game is ready...");
     }
   }, [racks]);
 
-  const stringToRack = (rackString) => {
+  const rackStringToArray = (rackString) => {
     if (rackString === undefined) rackString = "";
     const rackArray = ["", "", "", "", "", "", ""];
     rackString.split("").map((letter, index) => (rackArray[index] = letter));
@@ -69,25 +55,24 @@ const Rack = (props) => {
   };
 
   const getNewTiles = () => {
-    const selectedIndices = gameData.currentPlayer == 0 ? [0, 1, 2, 3, 4, 5, 6] : Array.from(rackSelectedIndices);
-
-    const newTiles = drawFromBag(props.tileBag, gameData.tilesRemaining, selectedIndices.length);
+    const selectedTilesIndices = gameData.currentPlayer === 0 ? [0, 1, 2, 3, 4, 5, 6] : Array.from(rackSelectedIndices);
+    const newTiles = drawFromBag(props.tileBag, gameData.tilesRemaining, selectedTilesIndices.length);
     const newTileCount = newTiles.length;
-    const newRack = stringToRack(racks[props.thisPlayer - 1]);
-    selectedIndices.map((i) => {
+    const newRack = rackStringToArray(racks[props.thisPlayer - 1]);
+    selectedTilesIndices.map(function (i) {
       newRack[i] = newTiles.pop();
     });
 
     const newRacks = [...racks];
     newRacks[props.thisPlayer - 1] = newRack.join("");
-    FirestoreDb.updateDatabaseState(props.gameId, "racks", newRacks);
+    FirestoreDb.updateState(props.gameId, "racks", newRacks);
     setRackSelectedIndices(new Set());
     console.log("Current player: ", gameData.currentPlayer);
     const newPlayer =
-      gameData.currentPlayer == 0 ? gameData.currentPlayer : (gameData.currentPlayer % players.length) + 1;
+      gameData.currentPlayer === 0 ? gameData.currentPlayer : (gameData.currentPlayer % players.length) + 1;
     console.log("New player: ", newPlayer);
-    FirestoreDb.updateDatabaseState(props.gameId, "currentPlayer", newPlayer);
-    FirestoreDb.updateDatabaseState(props.gameId, "tilesRemaining", gameData.tilesRemaining - newTileCount);
+    FirestoreDb.updateState(props.gameId, "currentPlayer", newPlayer);
+    FirestoreDb.updateState(props.gameId, "tilesRemaining", gameData.tilesRemaining - newTileCount);
   };
 
   const toggleLetterSelected = (letterIndex) => {
@@ -99,7 +84,7 @@ const Rack = (props) => {
   };
 
   const canUpdate =
-    gameData.currentPlayer == props.thisPlayer || (gameData.currentPlayer == 0 && racks[props.thisPlayer - 1] == "");
+    gameData.currentPlayer === props.thisPlayer || (gameData.currentPlayer === 0 && racks[props.thisPlayer - 1] === "");
 
   return (
     <div>
@@ -110,7 +95,7 @@ const Rack = (props) => {
         <p>Tiles remaining: {gameData.tilesRemaining}</p>
       </div>
       <div id="rack">
-        {stringToRack(racks[props.thisPlayer - 1]).map((letter, index) => {
+        {rackStringToArray(racks[props.thisPlayer - 1]).map((letter, index) => {
           return (
             <span
               key={index}
