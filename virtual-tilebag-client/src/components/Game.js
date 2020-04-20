be58a5d13e98fc7bd690d7b66d8e45a69d965e9d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-20 18:20:09 +1200

    Two-way syncing works!

--- virtual-tilebag-client/src/components/Game.js
+++ virtual-tilebag-client/src/components/Game.js
@@ -31,12 +31,11 @@ export const Game = (props) => {
 const Rack = (props) => {
   console.log("Rack re-render");
   // State values
-  const [rack, setRack] = useState(["", "", "", "", "", "", ""]);
+  // const [rack, setRack] = useState(["", "", "", "", "", "", ""]);
   const [rackSelectedIndices, setRackSelectedIndices] = useState(new Set());
   const [gameData, setGameData] = useState({});
   const [players, setPlayers] = useState([]);
-  const [hasGameStarted, setHasGameStarted] = useState();
-  const [allRacks, setAllRacks] = useState([]);
+  const [racks, setRacks] = useState([]);
 
   // Listener for changes to database -> update local state
   useEffect(() => {
@@ -46,53 +45,49 @@ const Rack = (props) => {
         tilesRemaining: doc.data().tilesRemaining,
       });
       setPlayers(doc.data().players);
-      setRack(stringToRack(doc.data().racks[props.thisPlayer - 1]));
-      setHasGameStarted(doc.data().hasGameStarted);
-      setAllRacks(doc.data().racks);
+      // setRack(stringToRack(doc.data().racks[props.thisPlayer - 1]));
+      setRacks(doc.data().racks);
       console.log("Updating from database");
       // console.log(gameData);
     });
     return unsubscribe;
   }, []);
 
-  // Update Database on changes to Rack/Tile state
-  useEffect(() => {
-    console.log("Updating game in database...");
-    console.log(allRacks);
-    FirestoreDb.updateGame(props.gameId, props.thisPlayer, gameData, rack, allRacks, hasGameStarted);
-  }, [gameData]);
-
   // Update hasGameStarted based on all players' rack state
   useEffect(() => {
-    if (allRacks.filter((r) => r !== "").length === players.length) setHasGameStarted(true);
-  }, [allRacks]);
+    if (racks.filter((r) => r !== "").length === players.length && players.length > 0 && gameData.currentPlayer == 0) {
+      FirestoreDb.updateDatabaseState(props.gameId, "currentPlayer", 1);
+      console.log("Game is ready...");
+    }
+  }, [racks]);
 
   const stringToRack = (rackString) => {
+    if (rackString === undefined) rackString = "";
     const rackArray = ["", "", "", "", "", "", ""];
     rackString.split("").map((letter, index) => (rackArray[index] = letter));
     return rackArray;
   };
 
   const getNewTiles = () => {
-    // Exception for initial draw -- build array and don't advance player
-
-    const selectedIndices = hasGameStarted ? Array.from(rackSelectedIndices) : [0, 1, 2, 3, 4, 5, 6];
+    const selectedIndices = gameData.currentPlayer == 0 ? [0, 1, 2, 3, 4, 5, 6] : Array.from(rackSelectedIndices);
 
     const newTiles = drawFromBag(props.tileBag, gameData.tilesRemaining, selectedIndices.length);
     const newTileCount = newTiles.length;
-    const newRack = [...rack];
+    const newRack = stringToRack(racks[props.thisPlayer - 1]);
     selectedIndices.map((i) => {
       newRack[i] = newTiles.pop();
     });
-    // console.log(newRack);
-    setRack(newRack);
+
+    const newRacks = [...racks];
+    newRacks[props.thisPlayer - 1] = newRack.join("");
+    FirestoreDb.updateDatabaseState(props.gameId, "racks", newRacks);
     setRackSelectedIndices(new Set());
-    const newPlayer = hasGameStarted ? (gameData.currentPlayer % players.length) + 1 : gameData.currentPlayer;
-    setGameData({
-      ...gameData,
-      tilesRemaining: gameData.tilesRemaining - newTileCount,
-      currentPlayer: newPlayer,
-    });
+    console.log("Current player: ", gameData.currentPlayer);
+    const newPlayer =
+      gameData.currentPlayer == 0 ? gameData.currentPlayer : (gameData.currentPlayer % players.length) + 1;
+    console.log("New player: ", newPlayer);
+    FirestoreDb.updateDatabaseState(props.gameId, "currentPlayer", newPlayer);
+    FirestoreDb.updateDatabaseState(props.gameId, "tilesRemaining", gameData.tilesRemaining - newTileCount);
   };
 
   const toggleLetterSelected = (letterIndex) => {
@@ -103,6 +98,9 @@ const Rack = (props) => {
     setRackSelectedIndices(tempSet);
   };
 
+  const canUpdate =
+    gameData.currentPlayer == props.thisPlayer || (gameData.currentPlayer == 0 && racks[props.thisPlayer - 1] == "");
+
   return (
     <div>
       <div id="info">
@@ -112,7 +110,7 @@ const Rack = (props) => {
         <p>Tiles remaining: {gameData.tilesRemaining}</p>
       </div>
       <div id="rack">
-        {rack.map((letter, index) => {
+        {stringToRack(racks[props.thisPlayer - 1]).map((letter, index) => {
           return (
             <span
               key={index}
@@ -124,8 +122,8 @@ const Rack = (props) => {
           );
         })}
       </div>
-      <button disabled={gameData.currentPlayer != props.thisPlayer && hasGameStarted} onClick={getNewTiles}>
-        Update Letters
+      <button disabled={!canUpdate} onClick={getNewTiles}>
+        Draw tiles from bag
       </button>
     </div>
   );
--- virtual-tilebag-client/src/services/tilebag.js
+++ virtual-tilebag-client/src/services/tilebag.js
@@ -2,7 +2,7 @@
 const shuffle = require("knuth-shuffle-seeded");
 
 // prettier-ignore
-const scrabbleEnglish = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z", "<BLANK>", "<BLANK>"];
+const scrabbleEnglish = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z", "_", "_"];
 
 export const shuffleBag = function (seed) {
   console.log("Shuffling the bag");
