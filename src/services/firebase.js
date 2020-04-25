7fea07dbaf1844b123ea9028a5fa67f45e3e6c15
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-04-26 00:50:12 +1200

    New firebase file without secret keys

---
+++ src/services/firebase.js
@@ -0,0 +1,47 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
+export const createGame = (players) => {
+  return db.collection("games").add({
+    startTime: firebase.firestore.FieldValue.serverTimestamp(),
+    currentPlayer: 0,
+    players: players,
+    racks: players.map((p) => ""),
+    // tilesRemaining: 100,
+    tileBag: [],
+    history: [],
+  });
+};
+
+export const getGame = (gameId) => {
+  return db.collection("games").doc(gameId).get();
+};
+
+export const updateState = (gameId, field, newValue) => {
+  console.log("Updating database...");
+  return db
+    .collection("games")
+    .doc(gameId)
+    .update({
+      [field]: newValue,
+    });
+};
+
+export const syncGameState = (gameId, doc) => {
+  console.log("syncing...");
+  return db.collection("games").doc(gameId).onSnapshot(doc);
+};
